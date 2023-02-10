fc356cfc5b67bd8428b34cc37bfbbff825b8082b
Josemaria Links <josemaria.links2@gmail.com>
2023-02-10 23:33:20 +0100

    feature: start working on the login.js file

---
+++ FrontEnd/login.js
@@ -0,0 +1,7 @@
+let email;
+let password;
+
+document.getElementById("submit").onclick = () => {
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+};
