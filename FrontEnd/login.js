a050ca5efa4cc2d17191affeadd051b963438e33
Josemaria Links <josemaria.links2@gmail.com>
2023-02-15 02:25:48 +0100

    feature: add working login in js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,7 +1,33 @@
-let email;
-let password;
+const loginForm = document.getElementById("connection");
 
-document.getElementById("submit").onclick = () => {
-  email = document.getElementById("email").value;
-  password = document.getElementById("password").value;
-};
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputEmail = document.getElementById("email").value;
+  const inputPassword = document.getElementById("password").value;
+
+  const inputLogin = { email: inputEmail, password: inputPassword };
+  const formDataString = JSON.stringify(inputLogin);
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/JSON",
+    },
+    body: formDataString,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((responseJson) => {
+      sessionStorage.setItem("token", responseJson.token);
+      location.href = "index.html";
+    })
+    .catch(() => {
+      // console.log(`erreur : ${error}`);
+      alert("Erreur, verifiez votre identifiant ou votre mot de passe");
+    });
+});
